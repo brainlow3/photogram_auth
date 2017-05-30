b7e982184dbf9ca3ec2525b2388c8c5ab89ea656
Brian Lowell <brian.lowell@gmail.com>
2017-05-29 20:27:38 -0500

    ..

--- config/routes.rb
+++ config/routes.rb
@@ -52,6 +52,9 @@ Rails.application.routes.draw do
   #------------------------------
 
   devise_for :users
+
+  get "/users", :controller => "users", :action => "index"
+  get "/users/:id", :controller => "users", :action => "show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
